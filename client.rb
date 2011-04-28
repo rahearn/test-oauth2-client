8f9fc668becffe3ff33f5f6df30d44e5866bd0a0
Ryan Ahearn <ryan@coshx.com>
2011-04-28 10:16:38 -0400

    Cleans up base client

--- client.rb
+++ client.rb
@@ -3,11 +3,17 @@ require 'oauth2'
 require 'json'
 
 def client
+  # app id, app secret, and site are all test-env specific
   OAuth2::Client.new('4db87a8a77d1eaa566000001', '56b257762db953aa4a1b36cfd16d3b1d33bb5005cc51f272f1c817f694ff7cff', :site => 'http://localhost:3000/')
 end
 
 get '/' do
   'working'
+end
+
+get '/logout' do
+  response.set_cookie 'access_token', ''
+  'logged out'
 end
 
 get '/auth' do
@@ -16,15 +22,16 @@ get '/auth' do
   )
 end
 
-get '/access' do
-  access_token = client.password.get_access_token(params[:username], params[:password])
-  user = JSON.parse(access_token.get('/current_user.json'))
+get '/auth/callback' do
+  access_token = client.web_server.get_access_token params[:code], :redirect_uri => redirect_uri
+  user = JSON.parse access_token.get('/current_user.json')
   user.inspect
 end
 
-get '/auth/callback' do
-  access_token = client.web_server.get_access_token(params[:code], :redirect_uri => redirect_uri)
-  user = JSON.parse(access_token.get('/current_user.json'))
+get '/access' do
+  access_token = client.password.get_access_token params[:username], params[:password]
+  response.set_cookie 'access_token', access_token.token
+  user = JSON.parse access_token.get('/current_user.json')
   user.inspect
 end
 
